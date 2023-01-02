2f7a26de95bb7e21ea56b69bbe3653c1ecdb727d
VatoLoco4ever <121635966+VatoLoco4ever@users.noreply.github.com>
2023-01-02 11:15:36 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := /device/wingtech/Newcastle/twrp_Newcastle
+LOCAL_PATH := device/wingtech/Newcastle/twrp_Newcastle
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
