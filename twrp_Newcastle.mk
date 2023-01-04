87ef2e6dbff428f115eedaef2ad6a37e83acc9c0
VatoLoco4ever <121635966+VatoLoco4ever@users.noreply.github.com>
2023-01-03 21:38:06 -0700

    Update twrp_Newcastle.mk

--- twrp_Newcastle.mk
+++ twrp_Newcastle.mk
@@ -1,9 +1,6 @@
-#
-# Copyright (C) 2022 The Android Open Source Project
-# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+
+# Inherit from Newcastle device
+$(call inherit-product, device/wingtech/Newcastle/device.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -11,9 +8,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-
-# Inherit from Newcastle device
-$(call inherit-product, device/wingtech/Newcastle/device.mk)
 
 PRODUCT_DEVICE := Newcastle
 PRODUCT_NAME := twrp_Newcastle
